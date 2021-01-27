be24cb4cfb6795ff1a53506a552641f82427e110
Sahn Lam <slam@users.noreply.github.com>
2021-01-26 22:34:30 -0800

    Switch the app to SwiftUI

---
+++ HongKongKeyboardApp/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  HongKongKeyboardApp
+//
+//  Created by Sahn Lam on 1/26/21.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
